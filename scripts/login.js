e9c47c00f7d1754a1417479c89d1f06cf5b246b9
RiazK <RiazCKhan@gmail.com>
2022-06-02 17:56:36 -0400

    moved login key stroke logic so that html is accesible

--- scripts/login.js
+++ scripts/login.js
@@ -23,17 +23,18 @@ $(document).ready(function () {
       alert("Incorrect Password")
     }
   })
+
+  // --- Login - Enter Key Stroke
+  let input = document.getElementById("password");
+  // let input = $('#password')
+  input.addEventListener("keyup", function (event) {
+    // Number 13 is the "Enter" key on the keyboard
+    if (event.keyCode === 13) {
+      // Cancel the default action, if needed
+      event.preventDefault();
+      // Trigger the button element with a click
+      document.getElementById("login-button").click();
+    }
+  });
+
 })
-
-// --- Disabled Login Enter Submit due to Error
-
-// var input = document.getElementById("password");
-// input.addEventListener("keyup", function(event) {
-//   // Number 13 is the "Enter" key on the keyboard
-//   if (event.keyCode === 13) {
-//     // Cancel the default action, if needed
-//     event.preventDefault();
-//     // Trigger the button element with a click
-//     document.getElementById("login-button").click();
-//   }
-// });
